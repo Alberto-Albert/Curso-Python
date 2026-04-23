09369c3726e422e2f780a9b1dcf6541766242896
Alberto Albert <alberto.albert.dev@gmail.com>
2026-04-23 12:51:22 +0200

    Funciones Basicas.py

---
+++ 08_funciones/funciones_basicas.py
@@ -0,0 +1,12 @@
+def mensaje():
+
+    print("Estamos aprendiendo Python")
+    print("Estamos aprendiendo instruciones básicas")
+    print("Poco a poco iremos avanzando")
+
+mensaje()
+
+mensaje()
+print("Ejecutando codigo fuera de función")
+
+mensaje()
